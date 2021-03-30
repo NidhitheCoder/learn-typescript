498f228a33433d53f5d729b97a2e34cf0d7de210
NidhitheCoder <nidhpv@gmail.com>
2021-03-30 15:24:24 +0530

    render props

--- reactWIthTypescript/sample-app/src/App.tsx
+++ reactWIthTypescript/sample-app/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Counter } from './Counter';
 import { TextField } from './TextField';
 
 const App :React.FC = () =>{
@@ -6,6 +7,12 @@ const App :React.FC = () =>{
     <TextField text="Hello" person={{firstName:"Sara",lastName:"Smith"}} 
     handleChange={(e)=>alert(e.target.value)}
     />
+    <Counter>
+      {({count,setCount}) =>(<div>
+        {count}
+        <button onClick={()=>setCount(count+1)} > + </button>
+        </div>)}
+    </Counter>
   </div>;
 }
 
